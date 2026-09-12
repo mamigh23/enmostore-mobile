324cdd38e3049a6d16cb30a74afcc62d52898b26
Muhammed <155327577+mamigh23@users.noreply.github.com>
2026-09-12 12:52:03 +0300

    fix: make Android package discovery explicit

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+/** @type {import('@react-native-community/cli-types').Config} */
+module.exports = {
+  project: {
+    android: {
+      packageName: 'com.enmostore.mobile',
+    },
+    ios: {
+      automaticPodsInstallation: true,
+    },
+  },
+};
